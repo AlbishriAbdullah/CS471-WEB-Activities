0f6c7bbafbb14f7b44428471cc8e2230dcd926c3
unknown <Abdullah.Albishri@outlook.com>
2024-09-19 13:11:41 +0300

    lab 4 till task 4

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 def index(request):
     name = request.GET.get("name") or "world!" #add this line
-    return render(request, "bookmodule/index.html" , {"name": name}) #your render line
-    
+    return HttpResponse("Hello, "+name) #replace the word “world!” with the variable name
+
+
 def index2(request, val1 = 0): #add the view function (index2)
     return HttpResponse("value1 = "+str(val1))
